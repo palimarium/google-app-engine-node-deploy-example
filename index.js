cd2107c8142ee23a9e7f56846bd3a4ce69217861
Marius Palimariu <palimarium@gmail.com>
2021-06-07 13:37:21 +0300

    push changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", async (req, res) => {
-  res.json({ status: "OK" });
+  res.json({ status: "OK1" });
 });
 
 app.listen(port, () => {
